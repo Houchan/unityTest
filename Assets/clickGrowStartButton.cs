551325aaa68987ca4025c09069862e79f603d801
Yusaku Matsuda <yusakumatuda1116@gmail.com>
2015-07-18 08:12:57 +0900

    成長シーンの画面を作成

---
+++ Assets/clickGrowStartButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class clickGrowStartButton : MonoBehaviour{
+	//
+	public GameObject growButton,clickStompButton,eatCauldronButton,blockTrackButton,backButtom;
+	public int plusstamina, plusweight, plusmusclel;
+
+
+	//
+	public void clickGrowButton(){
+		//育成ボタンを押したら各練習のボタンを表示
+		growButton.SetActive (false);
+		clickStompButton.SetActive(true);
+		eatCauldronButton.SetActive(true);
+		blockTrackButton.SetActive(true);
+	}
+
+	//ホーム画面に戻る
+	public void backHomeButton(){
+
+	}
+
+	//練習画面に戻る
+	public void backMenuButton(){
+		growButton.SetActive (true);
+		clickStompButton.SetActive(false);
+		eatCauldronButton.SetActive(false);
+		blockTrackButton.SetActive(false);
+	}
+}

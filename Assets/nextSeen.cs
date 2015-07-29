fdbb57a2069c77d6247c1cf716a449fa82186273
Yusaku Matsuda <yusakumatuda1116@gmail.com>
2015-07-30 00:15:33 +0900

    タイトルシーン、ゲームメインの作成

---
+++ Assets/nextSeen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextSeen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void nextGameMainSeen(){
+		Application.LoadLevel (5);
+	}
+
+	public void nextPracticeSeen(){
+		Application.LoadLevel (0);
+
+	}
+
+}

e992896db6204c8650cb699ef65419abee332fdb
gou nakamura <nakamura0422gou@gmail.com>
2015-07-30 00:12:59 +0900

    じゃんけん機能できました

---
+++ Assets/stratjanken.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+	
+public class stratjanken : MonoBehaviour {
+		
+	public bool flgJanken;
+	//int modeJanken = 0;　// jankenの状態を管理する
+
+	public GameObject startjankenobj;
+		
+	const int JANKEN = 0; //constはfinalと一緒
+	const int SCISSORS = 1;
+	const int STONE = 2;
+	const int PAPER = 3;
+	const int DRAW = 4;
+	const int WIN = 5;
+	const int LOOSE = 6;
+		
+	int myhand;
+	int comhand;
+		
+	int flgResult; //勝敗結果を保持
+
+	public void start(){
+		flgJanken = false;
+	}
+		
+	public void StartJankenbutton(){
+		// startjankenが押されたときに動きたい
+		flgJanken = true;
+	}
+		
+	
+	public void stonebutton(){
+		if (flgJanken == true) {
+			myhand = STONE;
+			judge();
+		}
+	}
+
+	
+	public void paperbutton(){
+		if (flgJanken == true) {
+			myhand = PAPER;
+			judge();
+		}
+	}
+
+	
+	public void scissorsbutton(){
+		if (flgJanken == true) {
+			myhand = SCISSORS;
+			judge();
+		}
+	}
+
+	void judge(){
+		comhand = Random.Range(STONE,PAPER+1);
+		//comの手を決める上の場合ランダム
+		
+		if(myhand == comhand){
+			flgResult = DRAW;
+		}else{
+			switch(comhand){
+			case STONE:
+				if(myhand == PAPER){
+					flgResult = LOOSE;
+				}
+				break;
+				
+			case SCISSORS:
+				if(myhand == STONE){
+					flgResult = LOOSE;
+				}
+				break;
+				
+			case PAPER:
+				if(myhand == SCISSORS){
+					flgResult = LOOSE;
+				}
+				break;
+			}
+			
+			if(flgResult != LOOSE){
+				flgResult = WIN;
+			}
+		}
+		Debug.Log(flgResult);
+	}
+
+	/*void Update(){
+	if (flgJanken == true) {
+		switch (modeJanken) {
+		case 0: //じゃんけん開始
+			modeJanken++;
+			break;
+
+		case 1: //プレイヤー入力待ち
+			if(modeJanken == 1){
+				//もしぐーボタンが押されたら
+				myhand = STONE;
+				modeJanken++;
+
+				//もしぱーボタンが押されたら
+				myhand = PAPER;
+				modeJanken++;
+
+				//もしちょきボタンが押されたら
+				myhand = SCISSORS;
+				modeJanken++;
+			}
+			break;
+
+		case 2: //判定
+			comhand = Random.Range(STONE,PAPER+1);
+			//comの手を決める上の場合ランダム
+
+			if(myhand == comhand){
+				flgResult = DRAW;
+			}else{
+				switch(comhand){
+				case STONE:
+					if(myhand == PAPER){
+						flgResult = LOOSE;
+					}
+					break;
+
+				case SCISSORS:
+					if(myhand == STONE){
+						flgResult = LOOSE;
+					}
+					break;
+						
+				case PAPER:
+					if(myhand == SCISSORS){
+						flgResult = LOOSE;
+					}
+					break;
+				}
+
+				if(flgResult != LOOSE){
+					flgResult = WIN;
+				}
+			}
+			modeJanken++;
+			break;
+
+			case 3: //結果
+
+
+
+				modeJanken++;
+				break;
+			
+			case 4: //じゃんけん終了
+				flgJanken = false;
+				modeJanken = 0;
+				break;
+			}
+		}
+	}*/
+}
